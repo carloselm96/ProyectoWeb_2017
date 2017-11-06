de29f49d2a8d8f9c7ec1cbf71916dde524a7018e
CARLOS ERNESTO LOPEZ MIJANGOS <zS14001379@estudiantes.uv.mx>
2017-11-06 16:33:17 -0600

    Programa inicial

---
+++ ProyectoWeb_2017/ProyectoWeb_2017/Models/Disciplina.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoWeb_2017.Models
+{
+    public class Disciplina
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+    }
+}
---
+++ ProyectoWeb_2017/ProyectoWeb_2017/Models/Equipo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProyectoWeb_2017.Models
+{
+    public class Equipo
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public string ciudad { get; set; }
+    }
+}
